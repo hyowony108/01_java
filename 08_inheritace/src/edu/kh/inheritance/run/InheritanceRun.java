cb741ea7ff392de1b1d52b60669bbb4cbda92152
hyowony108 <hyowony108@gmail.com>
2025-02-25 17:44:02 +0900

    상속

---
+++ 08_inheritace/src/edu/kh/inheritance/run/InheritanceRun.java
@@ -0,0 +1,22 @@
+package edu.kh.inheritance.run;
+
+import edu.kh.inheritance.model.dto.Employee;
+import edu.kh.inheritance.model.dto.Student;
+import edu.kh.inheritance.model.sesrvice.InheritanceService;
+
+public class InheritanceRun {
+
+	public static void main(String[] args) {
+
+		InheritanceService service = new InheritanceService();
+		//service.ex1();
+		//service.ex2();
+		//service.ex3();
+		service.ex4();
+		
+		
+		
+		
+	}
+
+}
